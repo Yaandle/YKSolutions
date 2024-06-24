d4fdacd2953952b5bc80d8b3175c0d85042ff50c
Yaandle <zyandle03@gmail.com>
2024-06-24 20:35:18 +1000

    Service Page

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '@/components/global/navbar';
 
-
-type Props = { children: React.ReactNode }
+type Props = { children: React.ReactNode };
 
 const Layout = (props: Props) => {
   return (
-    <div className="flex overflow-hidden h-screen">
-
-      <div className="w-full">
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow overflow-y-auto">
         {props.children}
-      </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
